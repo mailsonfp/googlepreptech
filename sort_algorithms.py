7698d67e3e7416b6b50cb11215c8672f67d4852c
mailsonfp <mailsonfp.dev@gmail.com>
2024-09-02 22:58:21 -0300

    aula 02-09-24

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -1,7 +1,7 @@
 # complexity: O(N²)
 def selection_sort(array_to_sort):
     for i in range(len(array_to_sort)):
-        for j in range(i+1, len(array_to_sort)):
+        for j in range(i + 1, len(array_to_sort)):
             if array_to_sort[j] < array_to_sort[i]:
                 array_to_sort[i], array_to_sort[j] = array_to_sort[j], array_to_sort[i]
 
@@ -13,8 +13,8 @@ def buble_sort(array_to_sort):
     for i in range(len(array_to_sort)):
         any_swap = False
         for j in range(i):
-            if array_to_sort[j] > array_to_sort[j+1]:
-                array_to_sort[j], array_to_sort[j+1] = array_to_sort[j+1], array_to_sort[j]
+            if array_to_sort[j] > array_to_sort[j + 1]:
+                array_to_sort[j], array_to_sort[j + 1] = array_to_sort[j + 1], array_to_sort[j]
                 any_swap = True
 
         if not any_swap:
@@ -97,6 +97,22 @@ def partition(array_to_sort, left, right):
     return after
 
 
+def count_sort(array_to_sort):
+    array_count = [0] * len(array_to_sort)
+
+    for i in range(len(array_to_sort)):
+        array_count[array_to_sort[i]] += 1
+
+    index_sort = 0
+
+    for i in range(len(array_count)):
+        if array_count[i] > 0:
+            for j in range(array_count[i], 0, -1):
+                array_to_sort[index_sort] = i
+                index_sort += 1
+                array_count[i] -= 1
+
+
 if __name__ == '__main__':
     array = [85, 55, 42, 3, 8, 10, 79, 1]
     print(f"Array to sort: {array}")
@@ -121,3 +137,10 @@ if __name__ == '__main__':
     print(f"Array to sort: {array}")
     quick_sort(array)
     print(f"Array sorted quick_sort: {array}")
+
+    print('-' * 20)
+
+    array = [3, 1, 2, 1, 1, 3]
+    print(f"Array to sort: {array}")
+    count_sort(array)
+    print(f"Array sorted count_sort: {array}")
